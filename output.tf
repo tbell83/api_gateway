9cd08f01fe7d1bda6ef1e776cdd40bebba8e7faf
Thomas Bell <tbell@dramafever.com>
2018-04-13 15:14:14 -0400

    count ouptuts too

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "execution_arn" {
+  count = "${var.count"
   value = "${aws_api_gateway_deployment.apigw.execution_arn}"
 }
 
 output "base_url" {
+  count = "${var.count"
   value = "${aws_api_gateway_deployment.apigw.invoke_url}"
 }
