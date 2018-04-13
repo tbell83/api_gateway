2f8795d068576e646c09a187e404fea7d56749f0
Thomas Bell <tbell@dramafever.com>
2018-04-13 15:26:33 -0400

    cant count outputs

--- output.tf
+++ output.tf
@@ -1,9 +1,7 @@
 output "execution_arn" {
-  count = "${var.count}"
   value = "${aws_api_gateway_deployment.apigw.*.execution_arn}"
 }
 
 output "base_url" {
-  count = "${var.count}"
   value = "${aws_api_gateway_deployment.apigw.*.invoke_url}"
 }
