58e170663bffd45f504fde7e3a7b3b9f391a68f9
Tom Bell <tbell@dramafever.com>
2018-07-27 15:00:35 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -7,5 +7,5 @@ output "base_url" {
 }
 
 output "cloudfront_domain_name" {
-  value = "${aws_api_gateway_domain_name.domain.*.domain_name}"
+  value = "${aws_api_gateway_domain_name.domain.*.cloudfront_domain_name}"
 }
