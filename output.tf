2f6c8c1b53fce3f726493c4e883239c166b74ddc
Thomas Bell <tbell@dramafever.com>
2018-07-27 14:50:09 -0400

    export cf domain name

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "execution_arn" {
 output "base_url" {
   value = "${aws_api_gateway_deployment.apigw.*.invoke_url}"
 }
+
+output "cloudfront_domain_name" {
+  value = "${aws_api_gateway_domain_name.domain.*.domain_name}"
+}
